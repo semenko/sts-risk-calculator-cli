26def26d7dd38ad0538dfc74e01b9999cb3825b6
Nick Semenkovich <semenko@alum.mit.edu>
2022-06-16 16:37:24 -0500

    New way to store choices

--- sts-query.py
+++ sts-query.py
@@ -10,11 +10,11 @@
 # Source: https://github.com/semenko/sts-risk-calculator-cli
 
 import argparse
-import requests
 import time
 
-STS_API_URL = "https://riskcalc.sts.org/stswebriskcalc/v1/calculate/stsall"
+import requests
 
+STS_API_URL = "https://riskcalc.sts.org/stswebriskcalc/v1/calculate/stsall"
 
 def query_sts_api(argparse_vars_as_dict):
 
@@ -57,8 +57,17 @@ def main():
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     core_args = parser.add_argument_group("core")
-    core_args.add_argument('--procedure', dest="procid", type=str, choices=['CAB', 'AVR', 'MVR', 'AVR+CAB', 'MVR+CAB', 'MVRepair', 'MVRepair+CAB'], required=True)
-    core_args.add_argument('--age', dest="age", metavar='{1-110}', type=int, choices=range(1, 111))
+    procedure_dict = {
+        'CAB': 1,
+        'AVR': 2,
+        'MVR': 3,
+        'AVR+CAB': 4,
+        'MVR+CAB': 5,
+        'MVRepair': 6,
+        'MVRepair+CAB': 7,
+    }
+    core_args.add_argument('--procedure', dest="procid", type=lambda x: procedure_dict[x], metavar=list(procedure_dict.keys()), required=True)
+    core_args.add_argument('--age', dest="age", metavar='{1-110}', type=int, choices=range(1, 111), required=True)
     core_args.add_argument('--gender', dest="gender", type=int, choices=['Male', 'Female'])
 
     group_race = parser.add_argument_group('race')
@@ -71,8 +80,18 @@ def main():
     group_race.add_argument('--hispanic-latino', dest="ethnicity", action='store_const', const='Yes', default='')
 
     metadata = parser.add_argument_group('metadata')
-    metadata.add_argument('--payor', dest="payorprim", type=str, choices=['self', 'medicare', 'medicaid', 'commercial', 'hmo', 'military', 'non-US'])
-    metadata.add_argument('--supplemental-payor', dest="payorsecond", type=str, choices=['self', 'medicare', 'medicaid', 'commercial', 'hmo', 'military', 'non-US'])
+
+    payor_dict = {
+        'self': 'None / self',
+        'medicare': 'Medicare (includes commercially managed options)',
+        'medicaid': 'Medicaid (includes commercially managed options)',
+        'commercial': 'Commercial Health Insurance',
+        'hmo': 'Health Maintenance Organization',
+        'non-US': 'Non-U.S. Plan',
+        'other': 'Other',
+        }
+    metadata.add_argument('--payor', dest="payorprim", type=lambda x: payor_dict[x], metavar=list(payor_dict.keys()))
+    metadata.add_argument('--supplemental-payor', dest="payorsecond", type=lambda x: payor_dict[x], metavar=list(payor_dict.keys()))
     metadata.add_argument('--date', dest="surgdt", type=str, metavar="8/11/2017")
 
     biometrics = parser.add_argument_group('biometrics')
@@ -85,75 +104,32 @@ def main():
     # ?? Calculated BMI parameter calculatedbmi
 
     comorbidities = parser.add_argument_group('comorbidities')
-    comorbidities.add_argument('--', dest="dialysis", action='store_const', const='Yes', default='')
-    comorbidities.add_argument('--', dest="hypertn", action='store_const', const='Yes', default='')
-    comorbidities.add_argument('--', dest="immsupp", action='store_const', const='Yes', default='')
-    comorbidities.add_argument('--', dest="pvd", action='store_const', const='Yes', default='')
+    comorbidities.add_argument('--dialysis', dest="dialysis", action='store_const', const='Yes', default='')
+    comorbidities.add_argument('--hypertension', dest="hypertn", action='store_const', const='Yes', default='')
+    comorbidities.add_argument('--immunosuppressed', dest="immsupp", action='store_const', const='Yes', default='')
+    comorbidities.add_argument('--pvd', dest="pvd", action='store_const', const='Yes', default='')
 
-    comorbidities.add_argument('--', dest="cvd", action='store_const', const='Yes', default='')
+    comorbidities.add_argument('--cvd', dest="cvd", action='store_const', const='Yes', default='')
     # comorbidities.add_argument('--', dest="cvdtia", action='store_const', const='Yes', default='')
     # comorbidities.add_argument('--', dest="cvdpcarsurg", action='store_const', const='Yes', default='')
 
-    comorbidities.add_argument('--', dest="mediastrad", action='store_const', const='Yes', default='')
-    comorbidities.add_argument('--', dest="cancer", action='store_const', const='Yes', default='')
-    comorbidities.add_argument('--', dest="fhcad", action='store_const', const='Yes', default='')
-    comorbidities.add_argument('--', dest="slpapn", action='store_const', const='Yes', default='')
-    comorbidities.add_argument('--', dest="liverdis", action='store_const', const='Yes', default='')
-    comorbidities.add_argument('--', dest="unrespstat", action='store_const', const='Yes', default='')
-    comorbidities.add_argument('--', dest="syncope", action='store_const', const='Yes', default='')
+    comorbidities.add_argument('--mediastinal-radiation', dest="mediastrad", action='store_const', const='Yes', default='')
+    comorbidities.add_argument('--cancer', dest="cancer", action='store_const', const='Yes', default='')
+    comorbidities.add_argument('--fh-cad', dest="fhcad", action='store_const', const='Yes', default='')
+    comorbidities.add_argument('--sleep-apnea', dest="slpapn", action='store_const', const='Yes', default='')
+    comorbidities.add_argument('--liver-disease', dest="liverdis", action='store_const', const='Yes', default='')
+    comorbidities.add_argument('--unresponsive', dest="unrespstat", action='store_const', const='Yes', default='')
+    comorbidities.add_argument('--syncope', dest="syncope", action='store_const', const='Yes', default='')
 
-    comorbidities.add_argument('--', dest="diabetes", action='store_const', const='Yes', default='')
+    comorbidities.add_argument('--diabetes', dest="diabetes", action='store_const', const='Yes', default='')
     # diabctrl
-    comorbidities.add_argument('--', dest="infendo", action='store_const', const='Yes', default='')
+    comorbidities.add_argument('--endocarditis', dest="infendo", action='store_const', const='Yes', default='')
     # infendty
 
-    comorbidities.add_argument('--', dest="cva", action='store_const', const='Yes', default='')
+    comorbidities.add_argument('--cva', dest="cva", action='store_const', const='Yes', default='')
     # cvawhen
    
-
-
-    # Chronic lung disease
-    # Stenosis R Carotid
-    # Stenosis L carotid
-    # illicit drug use
-    # alcohol use
-    # pneumonia
-    # smoking use
-    # home o2
-    # previous cardiac interventions
-    # MI when
-    # heart failure timing
-    # NYHA Class
-    # At time angina
-    # cardiogenic sock
-    # afib
-    # afib type
-    # afluitter
-    # third deg avb
-    # second deg 
-    # sick sinus
-    # VT/VF
-    # inotropes
-    # ADP inh
-    # ACE/ARB
-    # BB
-    # Steroids
-    # GB2B3a
-    # Resusc
-    # Number diseas
-    # LAD involv percent
-    # EF
-    # AS
-    # MS
-    # AI
-    # MR
-    # TR
-    # AV etiology
-    # Incidence
-    # status
-    # IABP
-    # Catheter used
-    # ECMO
+    comorbidities.add_argument('--illicits', dest="ivdrugab", action='store_const', const='Yes', default='')
 
 
     args = parser.parse_args()
